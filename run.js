6966e4a12b48ae840ce0ef88bc86ac3add6ce17f
Chad Hatcher <hackenslacker@gmail.com>
2017-09-06 17:46:35 -0700

    (run): parameterize port number

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
+var port = 3002;
 var ogre = require('./')
-ogre.createServer().listen(3001)
-console.log('Ogre listening on port 3001')
+ogre.createServer().listen(port)
+console.log(`Ogre listening on port ${port}`)
